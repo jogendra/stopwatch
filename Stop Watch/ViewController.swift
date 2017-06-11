8df5181a770e6ef93b22449a46ddcef8f76f6b4c
imjog <imjog24@gmail.com>
2017-06-11 13:15:39 +0530

    Initial Commit

--- Stop Watch/ViewController.swift
+++ Stop Watch/ViewController.swift
@@ -9,17 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var timeScreen: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        timeScreen.layer.borderColor = UIColor.brown.cgColor
+        timeScreen.layer.borderWidth = 1.0
+        
+    }
+    
+    
+    @IBAction func start(_ sender: Any) {
+        
+        
+        
+    }
+    
+    @IBAction func pause(_ sender: Any) {
+        
+        
     }
 
+    @IBAction func reset(_ sender: Any) {
+        
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
 
+    
 
 }
 
